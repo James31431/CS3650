2fddc2fa29a433ff9ccd042c9a40b3af7d69cb9e
Jingxuan Liu <james314@login-el8.khoury.northeastern.edu>
2026-09-19 21:43:41 -0400

    modified hello.c

--- a1-work-main/hello.c
+++ a1-work-main/hello.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  printf("Hello, World!\n");
-
+    for (int i = 0; i < 20; i++) {
+        printf("Jingxuan\n");
+        printf("liu.jingxua@northeastern.edu\n");
+    }
   return 0;
 }
